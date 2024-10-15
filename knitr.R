b7fa47d90a1dbbec9cf7d095511a87aeba2a38f1
Adam Marton <adam@adammarton.com>
2024-10-15 13:23:02 -0400

    emplty plus knit

---
+++ knitr.R
@@ -0,0 +1,4 @@
+install.packages("knitr")
+knitr::purl(input = "/Users/adammarton/Documents/GitHub/election-results-2024/election_scraper_2024.Rmd", output = "/Users/adammarton/Documents/GitHub/election-results-2024/election_scraper_2024.R",documentation = 0)
+
+
